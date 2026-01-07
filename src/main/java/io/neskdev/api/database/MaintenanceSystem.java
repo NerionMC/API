4803d4d65dc9d2549152caf6009291c5bb0c1100
neskuik_k <giraudeau.malone@icloud.com>
2026-01-07 15:04:12 +0100

    fix: add méthods.

--- src/main/java/io/neskdev/api/database/MaintenanceSystem.java
+++ src/main/java/io/neskdev/api/database/MaintenanceSystem.java
@@ -1,4 +1,11 @@
 package io.neskdev.api.database;
 
 public interface MaintenanceSystem {
+
+    void isMaintenance();
+    void setMaintenance();
+    void addAllowedPlayer();
+    void removeAllowedPlayer();
+    void isAllowed();
+
 }
